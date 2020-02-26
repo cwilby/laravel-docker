095f0949699854d3ce10d46bad05296dceae520e
Cameron Wilby <cameron@wilby.com>
2020-02-25 22:25:43 -0800

    fix: use latest tymondesigns/jwt-auth version

--- templates/docker/workspace/docker-entrypoint.sh
+++ templates/docker/workspace/docker-entrypoint.sh
@@ -45,7 +45,7 @@ pushd /var/www
   if [ -z "${APP_KEY}" ]; then
     php artisan key:generate --force
 
-    composer require tymon/jwt-auth
+    composer require tymon/jwt-auth:1.0.0-rc.5
     php artisan vendor:publish --provider="Tymon\JWTAuth\Providers\LaravelServiceProvider"
     php artisan jwt:secret --force
 
